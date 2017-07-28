4ace264d4fa1265fa3485aa16a8fafa00e36ae51
johnmusiu <johnmusiu@gmail.com>
2017-07-28 03:45:03 +0300

    Updage: deleted unnecessary funtionality from the file

--- db_create.py
+++ db_create.py
@@ -1,11 +1,5 @@
-mod app import db
-from models import *
+from app.api import db
+from app.models import User, Bucketlist, Activities
 
 #Create the database and the database tables
 db.create_all()
-
-#insert data
-#db.session.add(User(1, "John Musiu", "jmusiu", "jmusiu@a.com", "passs"))
-
-#commit changes
-#db.session.commit()
